3519e8bdf140a7c98375a024f002d7ba4169955b
Botond Bertalan <bbotond90@yahoo.com>
2016-07-22 16:14:29 +0100

    minifications

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-//refactored to use tsc --outputFile instead of --module amd
-//import {ChangeClass} from "./ChangeClass";
-$(function () {
-    var cc = new CC.ChangeClass();
-});
-//# sourceMappingURL=index.js.map
+$(function(){new CC.ChangeClass});
