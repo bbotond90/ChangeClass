815571db807c5850774ab947ad149748fbd92b46
bbotond90 <bbotond90@yahoo.com>
2016-07-24 10:40:19 +0100

    Redundant files removed
    We only need one js file, so all previous js files are deleted.

--- build/changeclass.js
+++ build/changeclass.js
@@ -1,1 +1,116 @@
-var CC;!function(t){var e=function(){function t(t){this._attrName=t}return Object.defineProperty(t.prototype,"attrName",{get:function(){return this._attrName},enumerable:!0,configurable:!0}),Object.defineProperty(t.prototype,"attrQuery",{get:function(){return"["+this._attrName+"]"},enumerable:!0,configurable:!0}),Object.defineProperty(t.prototype,"$Obj",{get:function(){return $(this.attrQuery)},enumerable:!0,configurable:!0}),t}();t.Attribute=e}(CC||(CC={}));var CC;!function(t){var e=function(){function e(){this._appTemplateNameAttribute=new t.Attribute("change-class-template"),this._optionNameContainerAttribute=new t.Attribute("class-option-name"),this._classContainerAttribute=new t.Attribute("class-container"),this._changeToButtonAttribute=new t.Attribute("change-class-to"),this._targetAttributePrefix="change-class-",this.registerEvents(),console.log("I am constructed")}return e.prototype.registerEvents=function(){this.onChangeToButtonAttributeClick()},e.prototype.onChangeToButtonAttributeClick=function(){var t=this;this._changeToButtonAttribute.$Obj.on("click",function(e){var r=$(e.target),n=""+r.attr(t._changeToButtonAttribute.attrName),a=t.targetAttributeConstructor(n);a.$Obj.each(function(t,e){var r=$(e),n=r.attr(a.attrName);r.removeClass(),r.addClass(n)})})},e.prototype.targetAttributeConstructor=function(e){return new t.Attribute(this._targetAttributePrefix+e)},e}();t.ChangeClass=e}(CC||(CC={})),$(function(){new CC.ChangeClass});
+//refactored to use tsc --outputFile instead of --module amd
+/// <reference path="lib/jquery.d.ts" />
+var CC;
+(function (CC) {
+    var Attribute = (function () {
+        function Attribute(name) {
+            this._attrName = name;
+        }
+        Object.defineProperty(Attribute.prototype, "attrName", {
+            /**
+            * Returns with its name alone
+            */
+            get: function () {
+                return this._attrName;
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(Attribute.prototype, "attrQuery", {
+            /**
+            * Shortcut for convinience.
+            * Returns with: [attrName]
+            */
+            get: function () {
+                return "[" + this._attrName + "]";
+            },
+            enumerable: true,
+            configurable: true
+        });
+        Object.defineProperty(Attribute.prototype, "$Obj", {
+            /**
+            * returns with $('[attrName]')
+            */
+            get: function () {
+                return $(this.attrQuery);
+            },
+            enumerable: true,
+            configurable: true
+        });
+        return Attribute;
+    }());
+    CC.Attribute = Attribute;
+})(CC || (CC = {}));
+/// <reference path="lib/require.d.ts" />
+/// <reference path="lib/jquery.d.ts" />
+//refactored to use tsc --outputFile instead of --module amd
+//import {Attribute} from "./Attribute";
+var CC;
+(function (CC) {
+    var ChangeClass = (function () {
+        function ChangeClass() {
+            /**
+            * appTemplateNameAttribute only denotes a scope of HTML where the app should change the classes on the selected elements.
+            * Where an html contains the 'change-class-template' attribute would only listen to events.
+            */
+            this._appTemplateNameAttribute = new CC.Attribute("change-class-template");
+            /**
+            * Must be a reference to one of the 'change-class-to' attribute's value.
+            * Example:
+            * <div change-class-to="GreenDesign">All Red</div> //this is the button that we listen to
+            *
+            <div class-option-name="GreenDesign">
+                This text will be green
+            </div>
+            */
+            this._optionNameContainerAttribute = new CC.Attribute("class-option-name");
+            this._classContainerAttribute = new CC.Attribute("class-container");
+            /**
+            * The changeToButtonAttribute's value have to contain one of the optionNameContainerAttribute's value.
+            */
+            this._changeToButtonAttribute = new CC.Attribute("change-class-to");
+            /**
+            * Attribute name prefix on containers.
+            */
+            this._targetAttributePrefix = "change-class-";
+            this.registerEvents();
+            console.log("I am constructed");
+        }
+        ChangeClass.prototype.registerEvents = function () {
+            this.onChangeToButtonAttributeClick();
+        };
+        /**
+         * onClick event on buttons marked with _changeToButtonAttribute. It will trigger a change on all
+         subscribed html element, which is subscribed on the button's container trigger,
+         */
+        ChangeClass.prototype.onChangeToButtonAttributeClick = function () {
+            var self = this;
+            this._changeToButtonAttribute.$Obj.on("click", function (e) {
+                var $target = $(e.target);
+                var targetContainerName = "" + $target.attr(self._changeToButtonAttribute.attrName);
+                var targetAttribute = self.targetAttributeConstructor(targetContainerName);
+                targetAttribute.$Obj.each(function (i, element) {
+                    var $e = $(element);
+                    var newClasses = $e.attr(targetAttribute.attrName);
+                    $e.removeClass();
+                    $e.addClass(newClasses);
+                });
+            });
+        };
+        /**
+         * Constructs the target Attribute which is subscribed for a button (has the requested container).
+         * @param targetContainerName The name of the targeted container/design name.
+         */
+        ChangeClass.prototype.targetAttributeConstructor = function (targetContainerName) {
+            return new CC.Attribute(this._targetAttributePrefix + targetContainerName);
+        };
+        return ChangeClass;
+    }());
+    CC.ChangeClass = ChangeClass;
+})(CC || (CC = {}));
+//refactored to use tsc --outputFile instead of --module amd
+//import {ChangeClass} from "./ChangeClass";
+$(function () {
+    var cc = new CC.ChangeClass();
+});
+//# sourceMappingURL=changeclass.js.map
